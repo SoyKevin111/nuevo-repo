88a2fe807c72e5bdc7fcc9f2fa9d00c950769e16
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-07 10:36:56 -0500

    subiendo una resta al proyecto

--- App.java
+++ App.java
@@ -1,12 +1,12 @@
 public class App {
 
-	public int sumaInt(int a, int b) {
-		return a + b;
+	public int RestaInt(int a, int b) {
+		return a - b;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		App a = new App();
-		System.out.println(a.sumaInt(10, 5));
+		System.out.println(a.RestaInt(10, 5));
 	}
 }
