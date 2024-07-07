c2f22f1e152fd0e841c7dbf2d5fd668baba0d00e
kevinStevenLC <kevinlema768xd@gmail.com>
2024-07-07 10:47:47 -0500

    generando 2 funciones (suma y resta)

--- App.java
+++ App.java
@@ -4,9 +4,14 @@ public class App {
 		return a - b;
 	}
 
+	public int SumaInt(int a, int b) {
+		return a + b;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		App a = new App();
 		System.out.println(a.RestaInt(10, 5));
+		System.out.println(a.SumaInt(10, 6));
 	}
 }
